2d84e659fa04cd1eed51fcbf6641c094cc0cc799
dainova <dainova@yahoo.com>
2015-12-10 21:35:25 -0800

    Update server.R
    Take2

--- server.R
+++ server.R
@@ -7,19 +7,44 @@ shinyServer(function(input, output) {
   # Return the requested dataset
   datasetInput <- reactive({
     switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
+           "Class" = rock,
+           "Sex" = pressure,
+           "Age" = cars)
   })
   
-  # Generate a summary of the dataset
+  
+  cascade<- reactive({         ##
+    t <- data.frame(Titanic)
+    temp <- quote(input$param1)
+    m <- unique(t[,eval(temp), drop=F])
+    sapply(m [, eval(temp)], as.character)
+  })
+  
+  #searchResult()[,1]
+  output$selectUI <- renderUI({ 
+    selectInput("value", "Choose a value:",  cascade())       
+  })
+  
+  
+  output$text <- renderText({ 
+    c("Selected Param: ", input$param1, "...value=", input$value )  })
+  
+  output$tbl <- renderTable({
+    t <- data.frame(Titanic)
+    #head(subset(t, Class =='1st'), n = input$obs)
+    head(subset(t,  get(input$param1) == input$value),n = input$obs)
+  })
+  #subset(t, Class ==inputvalue)    ## OK
+  #subset(t, get(inputparam) ==inputvalue)    ## <@>><   doesn't work !!!!!
+  #t[ t[colnames(t)==inputparam]==inputvalue, ]
+  #eval(parse( text=paste0("subset(t, ", inputparam, "=='", inputvalue, "')") ))
+  #t[ t[[inputparam]] ==inputvalue, ] 
+  
+  # Generate a summary if check box = TRUE
   output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-  
-  # Show the first "n" observations
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
+    if (input$summary)  ## == TRUE )
+    {
+    dataset <- data.frame(Titanic)
+    summary(dataset)  }
   })
 })
